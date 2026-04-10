cc55fbf01a01670010a3904841f177aecb51d5fb
Nizom <zoxidovnizom@gmail.com>
2026-04-10 09:46:57 +0500

    WHILE and DO WHILE

---
+++ lesson 8/index.js
@@ -0,0 +1,106 @@
+// 1. While
+// 2. do while
+
+
+// FOR loop
+// for (let raqam1 = 1; raqam1 <= 10; raqam1++) {
+//     console.log(raqam1);
+    
+// }
+
+// WHILE loop
+// let raqam2 = 1;
+// while (raqam2 <= 10) {
+//     console.log(raqam2);
+//     raqam2++    
+// }
+
+// DO WHILE loop
+// let raqam3 = 1;
+// do {
+//     console.log(raqam3);
+//     raqam3++;
+// } while (raqam3 <= 10);
+
+// =========================
+
+// let raqam1 = 1;
+// let yigindi = 0;
+// while (raqam1 <= 100) {
+//     yigindi += raqam1;
+//     raqam1++;
+// }
+// console.log(yigindi);
+
+// // =====================================
+// let raqam2 = 1;
+// let yigindi2 = 0;
+// do{
+//     yigindi2 += raqam2;
+//     raqam2++;
+// } while (raqam2 <= 100);
+// console.log(yigindi2);
+
+// =======================================
+
+
+// 3. 1 dan 50 gacha bo‘lgan juft sonlarni chiqaring.
+
+// let raqam1 = 1;
+// while ( raqam1 <= 50) {
+//     if (raqam1 % 2 === 0) console.log(raqam1);
+//     raqam1++
+// }
+// // ===============
+// let raqam2 = 1;
+// do{
+//     if (raqam2 % 2 === 0) console.log(raqam2);
+//     raqam2++
+// }while (raqam2 <= 50)
+
+
+// 3. 1 dan 50 gacha bo‘lgan juft sonlarni chiqaring.
+
+// let raqam1 = 1;
+// while ( raqam1 <= 50) {
+//     if (raqam1 % 2 === 1) console.log(raqam1);
+//     raqam1++
+// }
+// // ===============
+// let raqam2 = 1;
+// do{
+//     if (raqam2 % 2 === 1) console.log(raqam2);
+//     raqam2++
+// }while (raqam2 <= 50)
+
+// ===============================
+
+// let n =15;
+// let raqam1 = 1;
+// while(raqam1 <= n) {
+//     console.log(raqam1 * raqam1);
+//     raqam1++
+// }
+
+// // ===============
+
+// let raqam2 = 1;
+// do{
+//     console.log(raqam2 * raqam2);
+//     raqam2++
+// }while (raqam2 <= n)
+
+// 6. 1 dan N gacha bo‘lgan sonlar ichida 3 ga bo‘linadiganlarini chiqaring.
+
+// let n = 25;
+// let raqam1 = 1;
+// do{
+//     if (raqam1 % 3 ===0 ) console.log(raqam1);
+//     raqam1++
+// }while(raqam1 <= n);
+
+// let raqam2 = 1;
+// while(raqam2 <= n) {
+//     if (raqam2 % 3 ===0 ) console.log(raqam2);
+//     raqam2++
+// }

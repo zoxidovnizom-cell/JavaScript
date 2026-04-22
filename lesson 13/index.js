dee194f3a23c74b0d72d7a452867a7504da9a4b8
Nizom <zoxidovnizom@gmail.com>
2026-04-22 09:50:57 +0500

    biz bugun obj

---
+++ lesson 13/index.js
@@ -0,0 +1,50 @@
+//====================================
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// function eshmat(r1, r2, r3) {
+//     let summa = r1 + r2 + r3;
+//     return summa;
+// }
+// let sum = eshmat(a, b, c)
+// console.log(sum / 2);
+
+
+
+// const eshmat2 = function (r1, r2, r3) {
+//         let summa = r1 + r2 + r3;
+//     return summa;
+// }
+// let sum2 = eshmat2(a, b, c)
+// console.log(sum2 / 2);
+
+
+// const eshmat3 = (r1, r2, r3) => {
+//          let summa = r1 + r2 + r3;
+//     return summa;
+// }
+// let sum3 = eshmat3(a, b, c)
+// console.log(sum3 / 2);
+
+
+// ==========================================================
+
+// let obj = {
+//     ism: "Azizbek",
+//     familiya: "Abdukarimov",
+//     yosh: 14,
+//     isGoSchool: true,
+
+// };
+
+// // console.log(obj);
+
+// // 1. Static usul
+
+// console.log(obj.ism, obj.familiya);
+
+// // 2. Dinamic usul
+// console.log(obj['ism'], obj['familiya']);
+
+ 
